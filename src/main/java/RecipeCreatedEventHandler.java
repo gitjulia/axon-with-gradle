96cfc62278d26964a23cd06bc1280d749754d004
Julia Lee <jlee2@barclaycardus.com>
2015-06-15 22:25:26 -0400

    switch ElasticSearchRepository to plain CrudRepository. more flexible in case of changes to db implementation. also fixed event listener to populate query-side repository

--- src/main/java/RecipeCreatedEventHandler.java
+++ src/main/java/RecipeCreatedEventHandler.java
@@ -1,16 +1,16 @@
-//import org.springframework.stereotype.Component;
-//import static java.util.UUID.randomUUID;
-//
-//@Component
-//public class RecipeCreatedEventHandler {
-//
-//    private final RecipeRepository repository;
-//
-//    public RecipeCreatedEventHandler(RecipeRepository repository) {
-//        this.repository = repository;
-//    }
-//
-//    void on(RecipeCreatedEvent event) {
-//        repository.add(new RecipeView(event.getId()));
-//    }
-//}
+import org.springframework.stereotype.Component;
+import static java.util.UUID.randomUUID;
+
+@Component
+public class RecipeCreatedEventHandler {
+
+    private final RecipeViewRepository repository;
+
+    public RecipeCreatedEventHandler(RecipeViewRepository repository) {
+        this.repository = repository;
+    }
+
+    void on(RecipeCreatedEvent event) {
+        repository.add(new RecipeView(event.getId()));
+    }
+}
--- src/main/java/RecipeViewRepository.java
+++ src/main/java/RecipeViewRepository.java
@@ -1,6 +1,7 @@
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecipeViewRepository extends ElasticsearchRepository<RecipeView, String> {
+public interface RecipeViewRepository extends CrudRepository<RecipeView, String> {
+
 }
